3ba857b28f3b0b6d2eea54333805e4f75e74a438
Jim Bedenbaugh <megaskins@cox.net>
2011-03-09 18:30:11 -0600

    Bahahahaaaaaa.....

---
+++ ChessWeb/src/test/java/org/tjug/chess/GameTest.java
@@ -0,0 +1,34 @@
+package org.tjug.chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetOpponent1() {
+		fail("Gotcha all the way from Chicago! Bahahahahaaaaa...");
+	}
+
+	@Test
+	public void testGetOpponent2() {
+		fail("Gotcha all the way from Chicago! Bahahahahaaaaa...");
+	}
+
+	@Test
+	public void testGetStartTime() {
+		fail("Gotcha all the way from Chicago! Bahahahahaaaaa...");
+	}
+
+}
